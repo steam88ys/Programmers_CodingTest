6e1c98b6840409e5c042c9b9eee5079cd773dfb7
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-01 14:31:10 +0900

    없는 숫자 더하기_c

--- solution.c
+++ solution.c
@@ -2,13 +2,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// absolutes_len은 배열 absolutes의 길이입니다.
-// signs_len은 배열 signs의 길이입니다.
-int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
-    int answer = 0,i;
-    for(i = 0; i<absolutes_len; i++) {
-        if(signs[i]==false) answer -= absolutes[i];
-        else answer += absolutes[i];
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len) {
+    int answer = 45;
+    int i;
+    for(i = 0; i<numbers_len; i++) {
+        answer-=numbers[i];
     }
     return answer;
+    
 }
