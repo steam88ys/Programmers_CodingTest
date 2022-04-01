321201fbd11d701e440ab1629c461e72a5c5deb9
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-01 14:28:00 +0900

    없는 숫자 더하기_java

--- Solution.java
+++ Solution.java
@@ -1,11 +1,9 @@
 class Solution {
-    public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
-        for(int i = 0; i<absolutes.length; i++) {
-            if(signs[i]==false) answer -= absolutes[i];
-            else answer += absolutes[i];
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int i = 0; i<numbers.length; i++) {
+                answer-=numbers[i];
         }
-        
         return answer;
     }
 }
