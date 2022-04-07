5d98ce8f7689dcf0c0f2b2e7cafd82ca72b244e8
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-07 12:26:49 +0900

    두 정수 사이의 합_c

--- solution.c
+++ solution.c
@@ -2,13 +2,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// a_len은 배열 a의 길이입니다.
-// b_len은 배열 b의 길이입니다.
-int solution(int a[], size_t a_len, int b[], size_t b_len) {
-    int answer = 0;
-    int i;
-    for(i = 0; i<a_len; i++) {
-        answer += a[i] * b[i];
-    }
-    return answer;
+long long solution(int a, int b) {
+     long long answer = 0;
+        if(a>=b) {
+            for(int i = a; i>=b; i--) {
+                answer +=i;
+            }
+        }
+        else {
+            for(int i = b; i>=a; i--) {
+                answer +=i;
+            }
+        }
+        
+        return answer;
 }
