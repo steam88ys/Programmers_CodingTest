829ce43aff9076b34ea64abd3178f25b727553ae
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-11 11:32:00 +0900

    2016년_java

--- Solution.java
+++ Solution.java
@@ -1,9 +1,24 @@
 class Solution {
-    public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for(int i = 0; i<a.length; i++) {
-            answer += a[i]*b[i];
+    public String solution(int a, int b) {
+        
+        int month, day=0;
+        String[] d = new String[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        
+        for(month = 1; month<a; month++) {
+            if(month < 8) {
+                if(month==2) day += 29; // 2
+                else if(month%2==1) day += 31;  // (1, 3, 5, 7)
+                else day += 30; // (4, 6)
+            }
+            else {
+                if(month==2) day += 29; // 2
+                else if(month%2==1) day += 30;  // (9, 11)
+                else day += 31; // (8, 10, 12)
+            }
+            
         }
-        return answer;
+        day+=(b-1);
+    
+        return d[day%7];
     }
 }
