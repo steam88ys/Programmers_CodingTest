afe6e38b0d0e80ff0806d4c317e59ef8316c5b63
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-14 08:17:57 +0900

    부족한 금액 계산하기_java

--- Solution.java
+++ Solution.java
@@ -1,24 +1,9 @@
 class Solution {
-    public String solution(int a, int b) {
-        
-        int month, day=0;
-        String[] d = new String[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        
-        for(month = 1; month<a; month++) {
-            if(month < 8) {
-                if(month==2) day += 29; // 2
-                else if(month%2==1) day += 31;  // (1, 3, 5, 7)
-                else day += 30; // (4, 6)
-            }
-            else {
-                if(month==2) day += 29; // 2
-                else if(month%2==1) day += 30;  // (9, 11)
-                else day += 31; // (8, 10, 12)
-            }
-            
+    public long solution(int price, long money, int count) {
+        for(int i = 1; i<=count; i++) {
+            money -= (price*i);
         }
-        day+=(b-1);
-    
-        return d[day%7];
+        if(money >= 0) return 0;
+        else return money*(-1);
     }
 }
