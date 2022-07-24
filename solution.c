2ee2ab87672d1eea3024a14803556654013dd8a6
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-25 08:06:38 +0900

    자릿수 더하기

--- solution.c
+++ solution.c
@@ -2,18 +2,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-long long solution(int a, int b) {
-     long long answer = 0;
-        if(a>=b) {
-            for(int i = a; i>=b; i--) {
-                answer +=i;
-            }
-        }
-        else {
-            for(int i = b; i>=a; i--) {
-                answer +=i;
-            }
-        }
-        
-        return answer;
+int solution(int n) {
+    int answer = 0;
+
+    while(1) {
+        answer += n%10;
+        if(n!=0)n/=10;
+        else break;
+    }
+            
+    return answer;
 }
