e8d4d8cc321e75985a2e54068b613593e0980462
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-25 07:52:45 +0900

    짝수오 홀수

--- Solution.java
+++ Solution.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
-
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        for(int i=0; i<b; i++) {
-            for(int j = 0; j<a; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+    public String solution(int num) {
+        String answer = "";
+        if(num%2==0) answer = "Even";
+        else answer = "Odd";
+        return answer;
     }
 }
