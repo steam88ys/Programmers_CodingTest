d89abf4f3e4c029fbf2a067fe2673ae9da54780c
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-25 07:51:17 +0900

    직사각형 별찍기

--- Solution.java
+++ Solution.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; i<=n; i++) {
-            if(n%i==0) answer+=i;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=0; i<b; i++) {
+            for(int j = 0; j<a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        return answer;
     }
 }
