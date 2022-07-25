c577e85d3b16781eb74cdd60517d3104ad3c6969
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-26 08:23:13 +0900

    문자열을 정수로 바꾸기

--- solution.c
+++ solution.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num) {
-    long num2 = (long)num;
-    int answer = 0;
-    while(num2!=1) {
-        num2 = num2%2==0 ? num2/2 : (num2*3)+1;
-        ++answer;
-        if(answer>=500) return -1;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        boolean buho = true;
+        
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '-') {
+                buho = false;    
+            }else if(ch !='+'){;
+                answer = answer * 10 + (ch-'0');
+            }
+        }
+        return buho ? answer : (-1)*answer;
     }
-    return answer;
 }
