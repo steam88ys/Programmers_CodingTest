26a1e00411f100a6057f5da29e33bd1f0a75a6c2
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-26 07:42:58 +0900

    콜라츠 추측

--- solution.c
+++ solution.c
@@ -2,14 +2,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int n) {
+int solution(int num) {
+    long num2 = (long)num;
     int answer = 0;
-
-    while(1) {
-        answer += n%10;
-        if(n!=0)n/=10;
-        else break;
+    while(num2!=1) {
+        num2 = num2%2==0 ? num2/2 : (num2*3)+1;
+        ++answer;
+        if(answer>=500) return -1;
     }
-            
     return answer;
 }
