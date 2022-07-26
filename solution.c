e91fd0983cb5ce394d2d0faf8327ba2bb6dd2a2c
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-27 07:28:31 +0900

    핸드폰 번호 가리기

--- solution.c
+++ solution.c
@@ -2,10 +2,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-long long solution(long long n) {
-    double num = sqrt(n);
-    if(num == (int)num) {
-        return pow((num+1), 2);
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* phone_number) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char *answer = (char *)malloc(20);
+    int len = strlen(phone_number);
+    
+    strcpy(answer, phone_number);
+    for (int i = 0; i < len - 4; i++)
+    {
+        answer[i] = '*';
     }
-    else return -1;
+    return answer;
 }
