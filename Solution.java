203fe1fe6f7ab5358a6c681c1aec168bf36a2181
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-26 22:30:48 +0900

    가운데 글자 가져오기

--- Solution.java
+++ Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public double solution(int[] arr) {
-        double answer = 0;
-        for(int i=0; i<arr.length; i++) {
-            answer += arr[i];
-        }
-        return answer/arr.length;
+    public String solution(String s) {
+        String answer = "";
+        int idx = s.length()/2;
+        if(s.length()%2==0) answer = s.substring(idx-1, idx+1);
+        else answer = s.substring(idx, idx+1);
+        return answer;
     }
 }
