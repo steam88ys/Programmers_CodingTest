a2943e88c52f76c1fd4222f3d77ab6efed3eb1de
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-26 21:21:17 +0900

    하샤드 수

--- solution.c
+++ solution.c
@@ -1,16 +1,18 @@
-class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        boolean buho = true;
-        
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '-') {
-                buho = false;    
-            }else if(ch !='+'){;
-                answer = answer * 10 + (ch-'0');
-            }
-        }
-        return buho ? answer : (-1)*answer;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x) {
+    bool answer = false;
+    int origin = x;
+    int sum=0;
+    
+    while(x>0) {
+        sum+=(x%10);
+        x/=10;
     }
+    
+    if(origin%sum==0) answer = true;
+
+    return answer;
 }
