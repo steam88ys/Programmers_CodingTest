7ae1e620b7c4444bea375720a1afed70d6f4de69
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-28 07:47:38 +0900

    문자열 내림차순으로 배치하기

--- solution.c
+++ solution.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char* solution(const char* phone_number) {
+char* solution(const char* s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char *answer = (char *)malloc(20);
-    int len = strlen(phone_number);
+    char* answer = (char*)malloc(strlen(s));
+    strcpy(answer,s);
+    char temp=0;
     
-    strcpy(answer, phone_number);
-    for (int i = 0; i < len - 4; i++)
-    {
-        answer[i] = '*';
+    for(int i=0;i<strlen(s)+1;i++){
+        for(int j=i+1;j<strlen(s);j++){
+            if(answer[i]<answer[j]){
+                temp = answer[i];
+                answer[i] = answer[j];
+                answer[j] = temp;
+            }
+        }
     }
     return answer;
 }
