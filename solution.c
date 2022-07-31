a3a55b9211db8c78af04c3f1fcc1e49553a8cbc4
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-07-31 19:58:52 +0900

    자연수 뒤집어 배열로 만들기

--- solution.c
+++ solution.c
@@ -2,21 +2,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char* solution(const char* s) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc(strlen(s));
-    strcpy(answer,s);
-    char temp=0;
-    
-    for(int i=0;i<strlen(s)+1;i++){
-        for(int j=i+1;j<strlen(s);j++){
-            if(answer[i]<answer[j]){
-                temp = answer[i];
-                answer[i] = answer[j];
-                answer[j] = temp;
-            }
-        }
+int* solution(long long n) {
+    int len = 0;
+    long long temp = n;
+    while(temp!=0) {
+        temp/=10;
+        len++;
     }
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    int* answer = (int*)malloc(sizeof(int)*len);
+    temp =n;
+    
+    for(int i = 0; i<len; i++) {
+        answer[i] = temp%10;
+        temp /= 10;
+    }
+    
     return answer;
 }
